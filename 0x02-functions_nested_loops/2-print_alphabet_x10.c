ae1079049bc8d466ad4055a4956e1f880430ddd3
urjilaa <urji53210@gmail.com>
2023-08-16 00:07:55 -0700

    alx software

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "mmain.h"
+/**
+ * print_alphet_*10 -print the alphabet 10 times.
+ *
+ * Return: Always 0 (successs)
+ */
+void print_alphabet_*10(void)
+
+	int i;
+	int j;
+
+	for (j = 1; i <= 10; i++)
+        {
+	       for (j = 97; j <=122; j++)
+	       {
+		       _putchar(j)
+		}
+	        _putchar('\n')
+         }
+}		 
