7a2f85ff4a778a2922fc091e3628cceab42134f0
urjilaa <urji53210@gmail.com>
2023-08-16 07:45:19 -0700

    alx software

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,18 @@
-#include "mmain.h"
+#include "main.h"
 /**
- * print_alphet_*10 -print the alphabet 10 times.
- *
- * Return: Always 0 (successs)
- */
-void print_alphabet_*10(void)
-
-	int i;
-	int j;
-
-	for (j = 1; i <= 10; i++)
+*print_alphabet_x10 - prints alphabet letters
+* ten times
+*desc: The function uses loop to operate
+*Return: no return
+*/
+void print_alphabet_x10(void)
+{
+	int counter = 0;
+	int letter;
+	while (counter++ <= 9)
         {
-	       for (j = 97; j <=122; j++)
-	       {
-		       _putchar(j)
-		}
-	        _putchar('\n')
-         }
-}		 
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar(10);
+	}
+}
