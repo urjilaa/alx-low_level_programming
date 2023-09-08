efe69331c014d3986b0c7415014ad9054392a359
urjilaa <urji53210@gmail.com>
2023-09-08 09:50:32 -0700

    alx

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,13 +1,13 @@
-#include <stdlib.h>		
+#include <stdlib.h>
 #include "main.h"
 
-/**		
- * string_nconcat - concatenates n bytes of a string to another string		
+/**
+ * string_nconcat - concatenates n bytes of a string to another string
  * @s1: string to append to
  * @s2: string to concatenate from
  * @n: number of bytes from s2 to concatenate to s1
  *
- * Return: pointer to the resulting string	
+ * Return: pointer to the resulting string
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
@@ -16,7 +16,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *s;
 
 	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
-	
+
 	while (s1 && s1[len1])
 
 		len1++;
@@ -36,11 +36,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	while (i < len1)
 	{
-		s[i] = s1[i];	
+		s[i] = s1[i];
 		i++;
 	}
-	
-	while (n < len2 && i < (len1 + n))	
+
+	while (n < len2 && i < (len1 + n))
 		s[i++] = s2[j++];
 	while (n >= len2 && i < (len1 + len2))
 
@@ -48,5 +48,5 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	s[i] = '\0';
 
 	return (s);
-		
+
 }
