ce407ade7349b22da5e8ea8bbbd6fcd4e1d4b6a0
urjilaa <urji53210@gmail.com>
2023-09-08 10:20:41 -0700

    alx

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,7 +9,6 @@
  *
  * Return: pointer to the newly allocated memory block
  */
-
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *ptr1;
@@ -23,31 +22,29 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		free(ptr);
 		return (NULL);
+	}
+
 	if (!ptr)
-
 		return (malloc(new_size));
+
 	ptr1 = malloc(new_size);
-
 	if (!ptr1)
-
 		return (NULL);
+
 	old_ptr = ptr;
 
 	if (new_size < old_size)
 	{
-
 		for (i = 0; i < new_size; i++)
-
 			ptr1[i] = old_ptr[i];
 	}
 
 	if (new_size > old_size)
 	{
-
 		for (i = 0; i < old_size; i++)
-
 			ptr1[i] = old_ptr[i];
 	}
+
 	free(ptr);
 	return (ptr1);
 }
