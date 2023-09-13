314b15a19bed9f2001bf807e19a5a9249ba7b462
urjilaa <urji53210@gmail.com>
2023-09-13 07:31:28 -0700

    alx

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -22,7 +22,6 @@ struct dog
  */
 typedef struct dog dog_t;
 
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
@@ -31,5 +30,5 @@ char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 
 
-#endif                                                                                                                     
+#endif 
 ~                                               
