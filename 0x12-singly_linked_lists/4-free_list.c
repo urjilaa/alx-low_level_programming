48d3952d1075bc5bb8d56a1e9b4e317994fb4e55
urjilaa <urji53210@gmail.com>
2023-09-23 00:20:44 -0700

    alx

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "lists.h"
+
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
