6137609e07dd3c826168b03f6f928423c9a809b6
urjilaa <urji53210@gmail.com>
2023-09-27 12:08:02 -0700

    alx

--- 0x13-more_singly_linked_lists/list.h
+++ 0x13-more_singly_linked_lists/list.h
@@ -1,16 +1,19 @@
-#ifndef LISTS_H
-#define LISTS_H
-
+#ifndef _LISTS_
+#define _LISTS_
 #include <stdio.h>
 #include <stdlib.h>
-
 
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
-}
-listint_t;
+} listint_t;
+
+typedef struct listp_s
+{
+	void *p;
+	struct listp_s *next;
+} listp_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
