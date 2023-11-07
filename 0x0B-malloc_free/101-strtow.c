df8179262dbe8467a31c6b350f3efb57ca4ffcde
urjilaa <urji53210@gmail.com>
2023-11-07 12:17:34 -0800

    alx

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -17,34 +17,33 @@
 int count_word(char *s)
 
 {
+	int flag, c, w;
 
-        int flag, c, w;
+	flag = 0;
 
-        flag = 0;
+	w = 0;
 
-        w = 0;
+	for (c = 0; s[c] != '\0'; c++)
 
-        for (c = 0; s[c] != '\0'; c++)
+	{
 
-        {
+		if (s[c] == ' ')
 
-                if (s[c] == ' ')
+			flag = 0;
 
-                        flag = 0;
+		else if (flag == 0)
 
-                else if (flag == 0)
+		{
 
-                {
+			flag = 1;
 
-                        flag = 1;
+			w++;
 
-                        w++;
+		}
 
-                }
+	}
 
-        }
-
-        return (w);
+	return (w);
 
 }
 
@@ -66,70 +65,69 @@ char **strtow(char *str)
 
 {
 
-        char **matrix, *tmp;
+	char **matrix, *tmp;
 
-        int i, k = 0, len = 0, words, c = 0, start, end;
+	int i, k = 0, len = 0, words, c = 0, start, end;
 
-        while (*(str + len))
+	while (*(str + len))
 
-                len++;
+		len++;
 
-        words = count_word(str);
+	words = count_word(str);
 
-        if (words == 0)
+	if (words == 0)
 
-                return (NULL);
+		return (NULL);
 
-        matrix = (char **) malloc(sizeof(char *) * (words + 1));
+	matrix = (char **) malloc(sizeof(char *) * (words + 1));
 
-        if (matrix == NULL)
+	if (matrix == NULL)
 
-                return (NULL);
+		return (NULL);
 
-        for (i = 0; i <= len; i++)
+	for (i = 0; i <= len; i++)
 
-        {
+	{
 
-                if (str[i] == ' ' || str[i] == '\0')
+		if (str[i] == ' ' || str[i] == '\0')
 
-                {
+		{
+			if (c)
 
-                        if (c)
+			{
 
-                        {
+				end = i;
 
-                                end = i;
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
 
-                                tmp = (char *) malloc(sizeof(char) * (c + 1));
+				if (tmp == NULL)
 
-                                if (tmp == NULL)
+					return (NULL);
 
-                                        return (NULL);
+				while (start < end)
 
-                                while (start < end)
+					*tmp++ = str[start++];
 
-                                        *tmp++ = str[start++];
+				*tmp = '\0';
 
-                                *tmp = '\0';
+				matrix[k] = tmp - c;
 
-                                matrix[k] = tmp - c;
+				k++;
 
-                                k++;
+				c = 0;
 
-                                c = 0;
+			}
 
-                        }
+		}
 
-                }
+		else if (c++ == 0)
 
-                else if (c++ == 0)
+			start = i;
 
-                        start = i;
+	}
 
-        }
+	matrix[k] = NULL;
 
-        matrix[k] = NULL;
-
-        return (matrix);
+	return (matrix);
 
 }
