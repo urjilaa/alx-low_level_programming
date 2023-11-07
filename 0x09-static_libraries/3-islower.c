bb8e6b79cfe016d2daae2a4dde5e716e060771de
urjilaa <urji53210@gmail.com>
2023-11-07 10:14:48 -0800

    alx

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -9,18 +9,3 @@ int _islower(int c)
 {
 	return (c >= 'a' && c <= 'z');
 }
-
-0x09-static_libraries/3-puts.c
-#include "main.h"
-/**
- * _puts - prints a string, followed by a new line, to stdout
- * @str: string to print
- */
-void _puts(char *str)
-{
-	while (*str != '\0')
-	{
-		_putchar(*str++);
-	}
-		_putchar('\n');
-}
