cab753495f82b34bf6f2830a8b2feac393256449
urjilaa <urji53210@gmail.com>
2023-11-16 03:59:17 -0800

    alx

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *last;
+	list_t *temp = *head;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
+
+	last = malloc(sizeof(list_t));
+	if (!last)
+		return (NULL);
+
+	last->str = strdup(str);
+	last->len = len;
+	last->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = last;
+		return (last);
+	}
+
+	while (temp->next)
+		temp = temp->next;
+
+	temp->next = last;
+
+	return (last);
+}
