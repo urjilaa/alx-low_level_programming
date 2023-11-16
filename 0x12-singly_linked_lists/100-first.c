d93dbebf43a3a09daefda4d05d5f68cc3bf37976
urjilaa <urji53210@gmail.com>
2023-11-16 04:11:51 -0800

    alx

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,17 +1,7 @@
 #include <stdio.h>
 
 void new(void) __attribute__ ((constructor));
-
-void new(void)
-{
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
-}
-
-#include <stdio.h>
-
-void new(void) __attribute__ ((constructor));
-
+/*new*/
 void new(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
